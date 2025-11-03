2a0bfaff56c2e30a28f1b40241fee7b7a9bbe871
Adhamragap <adhamr192@gmail.com>
2025-11-03 16:56:22 +0200

    add scene delegete missing code

--- Shoppix/Application/Delegates/SceneDelegate.swift
+++ Shoppix/Application/Delegates/SceneDelegate.swift
@@ -13,8 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let firstVC = LandSceenViewController(nibName: "LandSceenViewController", bundle: nil)
+        let navController = UINavigationController(rootViewController: firstVC)
+        window.rootViewController = navController
+            window.makeKeyAndVisible()
+        self.window = window
+       
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
