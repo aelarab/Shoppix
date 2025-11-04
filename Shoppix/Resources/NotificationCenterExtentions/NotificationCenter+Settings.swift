7a212a17a086b528c7944be753b13e47c863526f
nafe333 <94410012+nafe333@users.noreply.github.com>
2025-11-04 18:08:06 +0200

    added search text field pod
    added notification center extensions file too

---
+++ Shoppix/Resources/NotificationCenterExtentions/NotificationCenter+Settings.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationCenter+Settings.swift
+//  Shoppix
+//
+//  Created by Nafea Elkassas on 04/11/2025.
+//
+
+import Foundation
+extension Notification.Name {
+    static let currencyDidChange = Notification.Name("currencyDidChange")
+}
+extension Notification.Name {
+    static let didAddNewAddress = Notification.Name("didAddNewAddress")
+}
