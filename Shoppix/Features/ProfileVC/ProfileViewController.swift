ab355fd4032925c45aa9f0bdaa761aac68f8a470
nafe333 <94410012+nafe333@users.noreply.github.com>
2025-11-05 04:07:44 +0200

    Profile ui added
    added profile vc ui and cells for later updates

---
+++ Shoppix/Features/ProfileVC/ProfileViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ProfileViewController.swift
+//  SHOPPIX
+//
+//  Created by Nafea Elkassas on 27/10/2025.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+       //MARK: - Outlets
+    
+    @IBOutlet weak var welcomeUsernameLabel: UILabel!
+    
+       //MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupnavBar()
+        
+    }
+
+       //MARK: - Behaviour
+    
+    func setupnavBar(){
+        navigationItem.title = "Me"
+
+        let cartButton = UIBarButtonItem(
+            image: UIImage(systemName: "cart"),
+            style: .plain,
+            target: self,
+            action: #selector(cartTapped)
+        )
+        
+        let favoriteButton = UIBarButtonItem(
+            image: UIImage(systemName: "gearshape"),
+            style: .plain,
+            target: self,
+            action: #selector(settingsTapped)
+        )
+        
+        if #available(iOS 16.0, *) {
+                navigationItem.trailingItemGroups = [
+                    UIBarButtonItemGroup(barButtonItems: [cartButton, favoriteButton], representativeItem: nil)
+                ]
+            } else {
+                navigationItem.rightBarButtonItems = [favoriteButton, cartButton]
+            }
+    }
+    
+    @objc func cartTapped(){
+        print("cart tapped")
+//        let cartVC = ShoppingCartViewController(nibName: "ShoppingCartViewController", bundle: nil)
+//        navigationController?.pushViewController(cartVC, animated: true)
+    }
+    
+    @objc func settingsTapped(){
+        let settingsVC = SettingsViewController(nibName: "SettingsViewController", bundle: nil)
+        navigationController?.pushViewController(settingsVC, animated: true)
+    }
+    
+       //MARK: - Actions
+    
+    @IBAction func moreOrdersButtonTapped(_ sender: UIButton) {
+        print("More orders tapped")
+    }
+    
+    @IBAction func moreWishlistButtonTapped(_ sender: UIButton) {
+        print("more wishlist tapped")
+    }
+}
