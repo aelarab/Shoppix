cfeb49a650794069eafc264c17ab9019f914302a
Adhamragap <adhamr192@gmail.com>
2025-11-09 02:00:54 +0200

    Stop tracking Pods locally

--- Pods/Target Support Files/Pods-Shoppix-ShoppixUITests/Pods-Shoppix-ShoppixUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shoppix_ShoppixUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Shoppix_ShoppixUITests
-@end
--- Pods/Target Support Files/Pods-Shoppix/Pods-Shoppix-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shoppix : NSObject
-@end
-@implementation PodsDummy_Pods_Shoppix
-@end
